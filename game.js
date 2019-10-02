7edec021fc2fc91ef46282555a8450732bd4f156
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-02 16:06:44 +0200

    selecting one hand

---
+++ game.js
@@ -0,0 +1,21 @@
+const hands = [...document.querySelectorAll('.select img')]
+const summaryOfGame = {
+  amount: 0,
+  wins: 0,
+  losses: 0,
+  draws: 0,
+}
+
+const choicesOfHand = {
+  playerHand: null,
+  computerHand: null,
+}
+
+function selectHand() {
+  choicesOfHand.playerHand = this.dataset.option
+  hands.forEach(hand => (hand.style.boxShadow = ''))
+  this.style.boxShadow = '0 0 0 4px black'
+}
+hands.forEach(hand => {
+  hand.addEventListener('click', selectHand)
+})
