76e9b41170f1814a98286fa2307428c99ba6a14b
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-02 16:19:24 +0200

    adding alert if option not selected

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 const hands = [...document.querySelectorAll('.select img')]
+const startGameButton = document.querySelector('.start')
+const information = document.querySelector('.information')
+
 const summaryOfGame = {
   amount: 0,
   wins: 0,
@@ -10,12 +13,19 @@ const choicesOfHand = {
   playerHand: null,
   computerHand: null,
 }
-
+function startGame() {
+  if (!choicesOfHand.playerHand) {
+    information.textContent = "Choose one of the hands !!"
+  }
+}
 function selectHand() {
   choicesOfHand.playerHand = this.dataset.option
   hands.forEach(hand => (hand.style.boxShadow = ''))
   this.style.boxShadow = '0 0 0 4px black'
 }
+
 hands.forEach(hand => {
   hand.addEventListener('click', selectHand)
 })
+
+startGameButton.addEventListener('click', startGame)
