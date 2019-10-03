7c92f06365b01c388ce3dcb6a7e37dfd5d2cd060
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-03 15:28:35 +0200

    function checkResult is working

--- game.js
+++ game.js
@@ -9,22 +9,35 @@ const summaryOfGame = {
   losses: 0,
   draws: 0,
 }
-function computerChoice() {
-  const index = Math.floor(Math.random() * 3)
-  choicesOfHand.computerHand = hands[index].dataset.option
-}
 
 const choicesOfHand = {
   playerHand: null,
   computerHand: null,
+}
+function computerChoice() {
+  const index = Math.floor(Math.random() * 3)
+  choicesOfHand.computerHand = hands[index].dataset.option
+}
+function checkResult(player, computer) {
+  if (player === computer) {
+    return "draw"
+  } else if
+    ((player === "paper" && computer === "rock")
+    || (player === "scissors" && computer === "paper")
+    || (player === "rock" && computer === "scissors")) {
+    return "win"
+  } else return "loss"
 }
 function startGame() {
   if (!choicesOfHand.playerHand) {
     return information.textContent = "Choose one of the hands !!"
   } else {
     computerChoice()
+    const gameResult = checkResult(choicesOfHand.playerHand, choicesOfHand.computerHand)
+    console.log(gameResult)
   }
 }
+
 function selectHand() {
   choicesOfHand.playerHand = this.dataset.option
   hands.forEach(hand => (hand.style.boxShadow = ''))
